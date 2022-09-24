a28c7e40117a0936268b38621188e0ac0908489e
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-09-24 10:11:19 -0700

    added route@6 to make different page accessable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Home from './pages/home/Home'
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, New, Single, List } from "./pages/pages";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div className="app">
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+
+        </Route>
+      </Routes>
     </div>
   );
+
 }
+
 
 export default App;
