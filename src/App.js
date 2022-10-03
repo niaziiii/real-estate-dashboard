392818c0c6cf41ce219e265da6199d311029760d
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-10-03 12:56:59 -0700

    wrap contexts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import { Home, Login, New, Single, List } from "./pages/pages";
 import './style.scss'
-import {AppContexts} from './contexts/AppContext'
+import { AppContexts } from './contexts/AppContext'
 import { useState } from "react";
 
 function App() {
-  const [background,setBackground]= useState(false);
-  const [name] = useState('alo')
+  const [background, setBackground] = useState(false);
   return (
-    <div className={background? 'app-dark' : "app"}>
-      <AppContexts.Provider value={{setBackground,name}}>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="requests" element={<List type={'product'}/>} />
+    <div className={background ? 'app-dark' : "app"}>
+      <AppContexts.Provider value={{ setBackground }}>
+        <Routes>
+          <Route path="/">
+            
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="requests" element={<List type={'product'} />} />
 
-          <Route path="property">
-            <Route index element={<List type={'property'} />} />
-            <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route path="property">
+              <Route index element={<List type={'property'} />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+
           </Route>
-
-        </Route>
-      </Routes>
+        </Routes>
       </AppContexts.Provider>
     </div>
   );
